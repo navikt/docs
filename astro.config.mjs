3f9ba114b8c28f645de4c1fdc2defeb53dddad6d
EIlif Johansen <eilifjohansen@outlook.com>
2024-09-18 15:19:38 +0200

    justerte config mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	base: '/',
 	integrations: [mdx(), sitemap()],
+	output: 'server',
 });
