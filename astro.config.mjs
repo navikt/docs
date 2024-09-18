cce5d8fcc35235225621b4dd8ed6e4f2bb691bc7
EIlif Johansen <eilifjohansen@outlook.com>
2024-09-18 15:33:57 +0200

    tok bort base: '/',

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/',
 	integrations: [mdx(), sitemap()],
 	output: 'server',
 	adapter: node({
