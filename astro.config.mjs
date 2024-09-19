f30d698be3e741e7652abb7a728001983a0f6104
EIlif Johansen <eilifjohansen@outlook.com>
2024-09-19 11:40:02 +0200

    Endret site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), sitemap()],
-	site: 'docs.ansatt.nav.no',
+	site: 'https://docs.ansatt.nav.no',
 	output: 'hybrid',
 	adapter: node({
 		mode: 'standalone',
