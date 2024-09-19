5d5be565e3ed10004a3010ad220952254b1c874c
EIlif Johansen <eilifjohansen@outlook.com>
2024-09-19 11:38:52 +0200

    La tilbake site i Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), sitemap()],
+	site: 'docs.ansatt.nav.no',
 	output: 'hybrid',
 	adapter: node({
 		mode: 'standalone',
