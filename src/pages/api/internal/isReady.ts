6a1674380d42c6a372aab11c3c6f06b15a83604c
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-09-19 12:46:32 +0200

    Opt-out av prerender på isAlive og isReady

--- src/pages/api/internal/isReady.ts
+++ src/pages/api/internal/isReady.ts
@@ -1,3 +1,7 @@
-export default async function handler(req, res) {
-    res.status(200).send('OK');
-}
+import type { APIRoute } from 'astro';
+
+export const prerender = false;
+
+export const GET: APIRoute = async function get() {
+    return new Response(null, { status: 200 });
+};
